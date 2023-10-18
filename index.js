eadde63c21e4b9bbb60c7e32b64e52d15a6aa1b7
henriquearaujo <henriquearaujo.ha85@gmail.com>
2023-10-17 21:48:40 -0300

    projeto criado

---
+++ index.js
@@ -0,0 +1,48 @@
+console.log ("digite o nome do seu jogador")
+let nickname = (" henrique90 ")
+console.log ( "bem vindo" + nickname )
+let nomeHeroi = ("destruidor")
+let sexo = ("M")
+let pontosDeVida = 100
+console.log (nomeHeroi)
+console.log (sexo)
+console.log (pontosDeVida)
+let xp = "10000"
+switch (xp){
+    case "1000":
+        console.log("ferro")
+        break
+    case "2000" :   
+    console.log("bronze")
+        break
+    case "4000" :
+        console.log("prata")
+        break
+    case "5000" :
+            console.log("ouro")
+        break
+    case "8000" :
+        console.log("platina/diamante")      
+        break
+    case "9000" :
+        console.log("ascendente")   
+        break
+    case "10000" :
+        console.log("imortal")
+        break
+    case "10001" :
+        console.log("radiante")  
+        break 
+
+
+}
+console.log (xp)
+
+
+
+
+
+
+
+
+
